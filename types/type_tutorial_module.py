9a4835c170469e2322abeccfcf39fd4bb1186635
yellowBirdy <mieszkomieszko@gmail.com>
2014-12-15 23:59:09 +0100

    Squashed commit of the following:
    commit 076cf0df1928dc10457debd6e07f6384cf29199c
    Author: yellowBirdy <mieszkomieszko@gmail.com>
    Date:   Mon Dec 15 17:44:44 2014 +0100
    
        Clean up after training
    
        - removed "simple" example folder due redundancy with pdscli example
        - changed type names in types/type_tutorial_module.py (consequence of replacing the "simple" example)
        - fixed namespace in /locations/query_tc.you.StopLocations_today.json

--- types/type_tutorial_module.py
+++ types/type_tutorial_module.py
@@ -15,8 +15,8 @@
 Type: personal
  
 Input:
-    activity:  /v1/data/tc.you.demo.SimpleActivityType
-    old_score:	/v1/data/tc.you.demo.SimpleActivityScore
+    activity:  /v1/data/tc.you.demo.CompilerTest
+    old_score:	/v1/data/tc.you.demo.ActivityScore
     
 
 Output:
@@ -28,5 +28,8 @@ def execute(activity, old_score, *args, **kwargs):
     scalarProperty = []	
     scalarProperty.append({'name': 'Example of a scalar type outupt in "stringP"', 'timestamp' : '2014-08-08T13:14:15Z', 'stringP': '1st SCALAR'})
     scalarProperty.append({'name': 'As you can see you can insert more then one data object at a time, still StringP is a scalar in each of the separate objects', 'timestamp' : '2014-09-09T14:13:11Z', 'stringP': '2st SCALAR'})	
+
     arrayProperty = {'name': 'Example of an array of scalars in a single property stringProperty. Note that it is still just a single object', 'timestamp' : '2014-11-11T13:14:15Z', 'stringProperty': ['This','is','an','array','of strings']}
+    
+    
     return dict(out1=scalarProperty, out2=[arrayProperty])
